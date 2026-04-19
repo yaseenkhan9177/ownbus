3923799c36814fafc6d2cb1e04838875d1dbf21d
Yaseen Khan <ykcaptain2223@gmail.com>
2026-04-19 10:31:08 +0500

    Fix HTTPS mixed content issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->web(append: [
             \App\Http\Middleware\SwitchTenantDatabase::class,
             \App\Http\Middleware\SetCompanyContext::class,
